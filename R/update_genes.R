fb46d5bd30abb809da70f95c33cd22f1f990efa2
ryanreis333 <ryanreis333@gmail.com>
2024-08-28 10:16:55 -0500

    fix

--- R/update_genes.R
+++ R/update_genes.R
@@ -53,7 +53,7 @@ all_genes <- unique(c(hgnc.table$Symbol, hgnc.table$Approved.Symbol))
 approved_genes <- unique(hgnc.table$Approved.Symbol)
 
 # Extract counts matrix from the specified assay in Seurat object
-mat <- Seurat_obj[[assay]]@counts
+mat <- Seurat_obj[[assay]]@layers$counts
 rownames(mat) <- rownames(Seurat_obj)
 
 # Filter genes with low counts and non-matching genes
